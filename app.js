290e0fcb00253b6afb4d79afbf047787559407e8
malcomcrab <malcomcrabesq@outlook.com>
2025-02-18 15:50:44 +0000

    Added validation/sanitation to the new user sign up form. Errors are displayed in the erros partial, within the page body

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
+const { body, validationResult } = require("express-validator");
 
 
 const app = express();
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,36 @@
 const queries = require("../queries")
+ // This just shows the new stuff we're adding to the existing contents
+ const { body, validationResult } = require("express-validator");
 
-exports.createUser = (req, res) => {
-    const {username,password} = req.body
-    queries.createNewUser(username, password);
-    res.redirect("/");
-      
-  };
-  
+ const alphaErr = "must only contain letters.";
+ const lengthErr = "must be between 1 and 10 characters.";
+ 
+ const validateUser = [
+   body("username").trim()
+     .isAlpha().withMessage(`First name ${alphaErr}`)
+     .isLength({ min: 1, max: 10 }).withMessage(`Username ${lengthErr}`),
+   body("password").trim()
+     .isAlpha().withMessage(`Last name ${alphaErr}`)
+     .isLength({ min: 1, max: 10 }).withMessage(`Password ${lengthErr}`),
+ ];
+ 
+ // We can pass an entire array of middleware validations to our controller.
+ 
+exports.createUser = [
+    validateUser,
+    (req, res) => {   
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).render("signUpPage", {
+            title: "signUpPage",
+            errors: errors.array(),
+          });
+        } 
+        const {username,password} = req.body
+        queries.createNewUser(username, password);
+        res.redirect("/");
+    }
+]
 
  
-  
+ 
