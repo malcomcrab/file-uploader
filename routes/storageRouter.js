b041b948ee3122497a4a0d30948dad4b967a5609
malcomcrab <malcomcrabesq@outlook.com>
2025-02-21 13:16:28 +0000

    Feature: Read function to get folders owned by the user and display as a list. Delete function: Each folder in the list has a delete form which finds and deletes the folder from the database.

--- routes/storageRouter.js
+++ routes/storageRouter.js
@@ -8,4 +8,6 @@ const upload = multer({ dest: 'uploads/' })
 
 storageRouter.post("/upload", upload.single('file'), storageController.postFile)
 storageRouter.post("/create-folder", storageController.createEmptyFolder)
+storageRouter.post("/:folder/delete-folder", storageController.deleteFolder)
+
 module.exports = storageRouter;
